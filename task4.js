99f9755af19bbff0ee6101cc9f8b8218ec37f9c5
unnu017 <aryamavila05@gmail.com>
2026-08-30 21:27:31 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,9 @@
+function getTimestamp() {
+    const now = new Date();
+    const pad = (n) => String(n).padStart(2, '0');
+
+    return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())} ` +
+           `${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
+}
+
+console.log('Current timestamp:', getTimestamp());
